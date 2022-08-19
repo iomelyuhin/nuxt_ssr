b37e8985b2ba02eab798b7a45f3c4f17241983a6
Ilya Omelyukhin <i.omelyuhin@gmail.com>
2022-08-19 10:41:42 +0300

    feat: add proxy when dev

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,4 @@
+const BASE_URL = process.env.NODE_ENV !== 'production' ? "https://site.com" : "/"
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -39,8 +40,13 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: BASE_URL,
+    credentials: false,
+    debug: false,
+    proxy: process.env.NODE_ENV !== 'production',
+  },
+  proxy: {
+    "/api/": `https://site.com`,
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
